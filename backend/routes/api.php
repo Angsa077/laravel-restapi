c35ad3c69959f209f0a3e20ed3e1186aee668fc4
Angga Saputra <angsa0077@gmail.com>
2023-09-19 13:28:05 +0700

    Sanctum Authentication & API Token (Login Logout API)

--- backend/routes/api.php
+++ backend/routes/api.php
@@ -1,8 +1,13 @@
 <?php
 
+use App\Http\Controllers\AuthenticationController;
 use App\Http\Controllers\postController;
 use Illuminate\Http\Request;
 use Illuminate\Support\Facades\Route;
 
-Route::get('/posts', [postController::class, 'index']);
-Route::get('/posts/{id}', [postController::class, 'show']);
+Route::get('/posts', [postController::class, 'index'])->middleware(['auth:sanctum']);
+Route::get('/posts/{id}', [postController::class, 'show'])->middleware(['auth:sanctum']);
+
+Route::post('/login', [AuthenticationController::class, 'login']);
+Route::post('/logout', [AuthenticationController::class, 'logout'])->middleware(['auth:sanctum']);
+Route::get('/me', [AuthenticationController::class, 'me'])->middleware(['auth:sanctum']);
